a5f7af312202fd752d0347d1ede61f1194f5466d
Jürgen Eidt <jurgene@microsoft.com>
2020-09-19 18:41:39 -0700

    update

--- app/src/main/java/com/thecloudsite/stockroom/invaders/InvadersActivity.kt
+++ app/src/main/java/com/thecloudsite/stockroom/invaders/InvadersActivity.kt
@@ -8,39 +8,42 @@ import android.os.Bundle
 
 class InvadersActivity : Activity() {
 
-    // InvadersView will be the view of the game
-    // It will also hold the logic of the game
-    // and respond to screen touches as well
-    private var invadersView: InvadersView? = null
+  // InvadersView will be the view of the game
+  // It will also hold the logic of the game
+  // and respond to screen touches as well
+  private var invadersView: InvadersView? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
 
-        // Get a Display object to access screen details
-        val display = windowManager.defaultDisplay
-        // Load the resolution into a Point object
-        val size = Point()
-        display.getSize(size)
+    // Get a Display object to access screen details
+    val display = windowManager.defaultDisplay
+    // Load the resolution into a Point object
+    val size = Point()
+    display.getSize(size)
 
-        // Initialize gameView and set it as the view
-        invadersView = InvadersView(this, size)
-        setContentView(invadersView)
+    // Initialize gameView and set it as the view
+    invadersView = InvadersView(this, size)
+    setContentView(invadersView)
+  }
+
+  // This method executes when the player starts the game
+  // Also called after onCreate
+  override fun onResume() {
+    try {
+      super.onResume()
+
+      // Tell the gameView resume method to execute
+      invadersView?.resume()
+    } catch (e: Exception) {
     }
+  }
 
-    // This method executes when the player starts the game
-    // Also called after onCreate
-    override fun onResume() {
-        super.onResume()
+  // This method executes when the player quits the game
+  override fun onPause() {
+    super.onPause()
 
-        // Tell the gameView resume method to execute
-        invadersView?.resume()
-    }
-
-    // This method executes when the player quits the game
-    override fun onPause() {
-        super.onPause()
-
-        // Tell the gameView pause method to execute
-        invadersView?.pause()
-    }
+    // Tell the gameView pause method to execute
+    invadersView?.pause()
+  }
 }
