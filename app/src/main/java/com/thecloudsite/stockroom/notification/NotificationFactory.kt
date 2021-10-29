6252bd1061ba4b980ed7fd315a95da9dc230f9d8
Jürgen Eidt <jurgene@microsoft.com>
2021-10-28 18:31:39 -0700

    .

--- app/src/main/java/com/thecloudsite/stockroom/notification/NotificationFactory.kt
+++ app/src/main/java/com/thecloudsite/stockroom/notification/NotificationFactory.kt
@@ -29,53 +29,57 @@ import com.thecloudsite.stockroom.StockDataActivity
 import java.util.concurrent.atomic.AtomicInteger
 
 object NotificationID {
-  private val c: AtomicInteger = AtomicInteger(1)
-  val Id: Int
-    get() = c.incrementAndGet()
+    private val c: AtomicInteger = AtomicInteger(1)
+    val Id: Int
+        get() = c.incrementAndGet()
 }
 
 class NotificationFactory(
-  val context: Context,
-  title: String,
-  text: String,
-  symbol: String
+    val context: Context,
+    title: String,
+    text: String,
+    symbol: String
 ) {
-  private val notificationId: Int = NotificationID.Id
+    private val notificationId: Int = NotificationID.Id
 
-  private val intent = Intent(context, StockDataActivity::class.java).apply {
-    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-    putExtra(EXTRA_SYMBOL, symbol)
-  }
-
-  private val pendingIntent: PendingIntent? = TaskStackBuilder.create(context)
-      .run {
-        // Add the intent, which inflates the back stack
-        addNextIntentWithParentStack(intent)
-        // Get the PendingIntent containing the entire back stack
-        getPendingIntent(notificationId, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
-      }
-
-  private val notificationBuilder: Builder =
-    Builder(context, NotificationChannelFactory.NotificationChannelId)
-        .setSmallIcon(drawable.ic_notification_chart)
-        .setContentTitle(
-            title
-        )
-        .setContentIntent(pendingIntent)
-        .setStyle(
-            NotificationCompat.BigTextStyle()
-                .bigText(text)
-        )
-        .setDefaults(
-            NotificationCompat.DEFAULT_SOUND or NotificationCompat.DEFAULT_VIBRATE
-        )
-        .setPriority(NotificationCompat.PRIORITY_MAX)
-        .setAutoCancel(true)
-
-  fun sendNotification() {
-    with(NotificationManagerCompat.from(context)) {
-      // NotificationId is a unique int for each notification.
-      notify(notificationId, notificationBuilder.build())
+    private val intent = Intent(context, StockDataActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        putExtra(EXTRA_SYMBOL, symbol)
     }
-  }
+
+    private val pendingIntent: PendingIntent? = TaskStackBuilder.create(context)
+        .run {
+            // Add the intent, which inflates the back stack
+            addNextIntentWithParentStack(intent)
+            // Get the PendingIntent containing the entire back stack
+            // FLAG_IMMUTABLE required by SDK 31
+            getPendingIntent(
+                notificationId,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+        }
+
+    private val notificationBuilder: Builder =
+        Builder(context, NotificationChannelFactory.NotificationChannelId)
+            .setSmallIcon(drawable.ic_notification_chart)
+            .setContentTitle(
+                title
+            )
+            .setContentIntent(pendingIntent)
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText(text)
+            )
+            .setDefaults(
+                NotificationCompat.DEFAULT_SOUND or NotificationCompat.DEFAULT_VIBRATE
+            )
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setAutoCancel(true)
+
+    fun sendNotification() {
+        with(NotificationManagerCompat.from(context)) {
+            // NotificationId is a unique int for each notification.
+            notify(notificationId, notificationBuilder.build())
+        }
+    }
 }
