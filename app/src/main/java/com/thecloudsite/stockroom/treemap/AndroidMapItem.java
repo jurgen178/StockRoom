b2802a44f72cb523426aa1f7f3a5139359cb74b3
Jürgen Eidt <jurgene@microsoft.com>
2021-12-06 22:12:36 -0800

    .

--- app/src/main/java/com/thecloudsite/stockroom/treemap/AndroidMapItem.java
+++ app/src/main/java/com/thecloudsite/stockroom/treemap/AndroidMapItem.java
@@ -15,91 +15,66 @@
  */
 package com.thecloudsite.stockroom.treemap;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import android.graphics.RectF;
-import org.jetbrains.annotations.NotNull;
 
-public class AndroidMapItem extends MapItem implements AndroidMappable, Comparable<AndroidMapItem> {
-  private double weight;
-  private final String symbol;
-  private final String label;
-  private final String text;
-  private final String change;
-  private Integer backgroundColor;
-  private Integer textColor;
-  private boolean groupColorsUsed;
+public class AndroidMapItem extends MapItem implements AndroidMappable {
+    private final String symbol;
+    private final String label;
+    private final String text;
+    private final String change;
+    private Integer backgroundColor;
+    private final Integer textColor;
+    private final boolean groupColorsUsed;
 
-  public AndroidMapItem(double weight, String symbol, String label, String text, String change, Integer backgroundColor, Integer textColor, boolean groupColorsUsed) {
-    setSize(weight);
-    this.weight = weight;
-    this.symbol = symbol;
-    this.label = label;
-    this.text = text;
-    this.change = change;
-    this.backgroundColor = backgroundColor;
-    this.textColor = textColor;
-    this.groupColorsUsed = groupColorsUsed;
-  }
+    public AndroidMapItem(double weight, String symbol, String label, String text, String change, Integer backgroundColor, Integer textColor, boolean groupColorsUsed) {
+        setSize(weight);
+        this.symbol = symbol;
+        this.label = label;
+        this.text = text;
+        this.change = change;
+        this.backgroundColor = backgroundColor;
+        this.textColor = textColor;
+        this.groupColorsUsed = groupColorsUsed;
+    }
 
-  public String getSymbol() {
-    return symbol;
-  }
+    public String getSymbol() {
+        return symbol;
+    }
 
-  public String getLabel() {
-    return label;
-  }
+    public String getLabel() {
+        return label;
+    }
 
-  public String getText() {
-    return text;
-  }
+    public String getText() {
+        return text;
+    }
 
-  public String getChange() {
-    return change;
-  }
+    public String getChange() {
+        return change;
+    }
 
-  public Integer getBackgroundColor() {
-    return backgroundColor;
-  }
+    public Integer getBackgroundColor() {
+        return backgroundColor;
+    }
 
-  public Integer getTextColor() {
-    return textColor;
-  }
+    public Integer getTextColor() {
+        return textColor;
+    }
 
-  public boolean getGroupColorsUsed() {
-    return groupColorsUsed;
-  }
+    public boolean getGroupColorsUsed() {
+        return groupColorsUsed;
+    }
 
-  public void setBackgroundColor(Integer backgroundColor) {
-    this.backgroundColor = backgroundColor;
-  }
+    public void setBackgroundColor(Integer backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
 
-  /* Return an Android RectF that is the size of the bounds rectangle */
-  public RectF getBoundsRectF() {
-    Rect bounds = getBounds();
-    return new RectF(Double.valueOf(bounds.x).floatValue(),
-        Double.valueOf(bounds.y).floatValue(),
-        Double.valueOf(bounds.x).floatValue() + Double.valueOf(bounds.w).floatValue(),
-        Double.valueOf(bounds.y).floatValue() + Double.valueOf(bounds.h).floatValue());
-  }
-
-  public static <T extends Comparable<? super T>> ArrayList<T> asReverseSortedList(
-      Collection<T> collection) {
-    ArrayList<T> arrayList = new ArrayList<>(collection);
-    arrayList.sort(Collections.reverseOrder());
-    return arrayList;
-  }
-
-  @Override
-  public int compareTo(AndroidMapItem otherItem) {
-    return Double.compare(weight, otherItem.weight);
-  }
-
-  @NotNull @Override
-  public String toString() {
-    return AndroidMapItem.class.getSimpleName() + "[label=" + label + ",weight=" + weight +
-        ",bounds=" + getBounds().toString() +
-        ",boundsRectF=" + getBoundsRectF().toString() + "]";
-  }
+    /* Return an Android RectF that is the size of the bounds rectangle */
+    public RectF getBoundsRectF() {
+        Rect bounds = getBounds();
+        return new RectF(Double.valueOf(bounds.x).floatValue(),
+                Double.valueOf(bounds.y).floatValue(),
+                Double.valueOf(bounds.x).floatValue() + Double.valueOf(bounds.w).floatValue(),
+                Double.valueOf(bounds.y).floatValue() + Double.valueOf(bounds.h).floatValue());
+    }
 }
