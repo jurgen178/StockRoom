5e4ec328e641350bfe960d062c6bc9c8eba6b6f6
Jürgen Eidt <jurgene@microsoft.com>
2023-05-05 17:34:40 -0700

    .

--- app/src/main/java/com/thecloudsite/stockroom/StockApiFactory.kt
+++ app/src/main/java/com/thecloudsite/stockroom/StockApiFactory.kt
@@ -340,7 +340,11 @@ object YahooCrumbDataApiFactory {
                     val original = chain.request()
                     val newRequest = original
                         .newBuilder()
-                        .addHeader("Accept", "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+                        //.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+//                        .addHeader("Host", "finance.yahoo.com")
+//                        .addHeader("User-Agent", "Android/10")
+//                        .addHeader("Accept", "text/html")
+//                        .addHeader("Accept-Language", "en")
                         .build()
                     chain.proceed(newRequest)
                 }
