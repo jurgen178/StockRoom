cd61f856ca0eb5bafb5f9888e08ff40da535e496
Jürgen Eidt <jurgene@microsoft.com>
2024-01-20 15:04:46 -0800

    update kotlin 1.9.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,3 @@
 plugins {
-    id("com.google.devtools.ksp").version("1.9.22-1.0.17") // Or latest version of KSP, see also https://github.com/square/moshi#codegen
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false // Or latest version of KSP, see also https://github.com/square/moshi#codegen
 }
