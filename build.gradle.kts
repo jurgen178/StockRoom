3d44fd55df9e4328f876393202f932bfded103b2
Jürgen Eidt <jurgene@microsoft.com>
2024-01-20 14:13:19 -0800

    update build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,3 @@
 plugins {
-    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+    id("com.google.devtools.ksp").version("1.9.22-1.0.17") // Or latest version of KSP, see also https://github.com/square/moshi#codegen
 }
